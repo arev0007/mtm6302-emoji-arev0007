4c712b3a246329d76893eb4a42f46518727c2839
Aldana <arev0007@algonquinlive.com>
2023-03-09 08:45:00 -0500

    Completes the assignment

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("click");
+const gallery = document.getElementById("container");
+button.addEventListener("click",() => {
+    gallery.style.visibility = "visible";
+});
+
+//Reference:
+//https://www.tutorialspoint.com/how-to-show-images-with-a-click-in-javascript-using-html
+//https://www.w3schools.com/jsref/prop_style_visibility.asp
