64dcd297b8aa20e39abdf5f752add0375fad856c
Aldana <arev0007@algonquinlive.com>
2023-03-09 16:08:38 -0500

    completes the assignment

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-const button = document.getElementById("click");
-const gallery = document.getElementById("container");
-button.addEventListener("click",() => {
-    gallery.style.visibility = "visible";
-});
+const $container = document.getElementById('container')
+const gallery = []
+
+for(let i = 129409; i< 129459 ; i++) {
+  gallery.push(`<p>
+  <span>&#${i};</span><br>
+  <code>${i}</code>
+</p>`) 
+}
+
+$container.innerHTML += gallery.join('') 
+
+
+
 
 //Reference:
 //https://www.tutorialspoint.com/how-to-show-images-with-a-click-in-javascript-using-html
 //https://www.w3schools.com/jsref/prop_style_visibility.asp
+// usar for loop p/ generar emojis
+// crear un array y un for of > tener mayor control + facil
